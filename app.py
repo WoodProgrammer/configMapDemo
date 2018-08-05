1da17da207d2e0759cddced558c2334b159df793
Emir Özbir <emirozbir@hotmail.com>
2018-08-05 18:33:32 +0300

    New Method added

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import os
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/v1/index")
 def index():
     env1 = os.getenv("API_KEY")
     env2 = os.getenv("HEAP_SIZE")
     return env1 + env2 
 
+@app.route("/v1"):
+def doc():
+    return "{'/v1/index'}"
 
 
 if __name__ == "__main__":
